d3605c84106c7cd5f07a77443393fd6d34c33e5d
mitermitek <188499468+mitermitek@users.noreply.github.com>
2026-09-06 16:01:52 +0200

    feat: add Scalar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using wiki_timeline_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
